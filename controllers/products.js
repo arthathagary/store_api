832bd1720deb54a4f033a37ef5cb95c24688a249
Arthath Agary <arthathagary@gmail.com>
2023-07-02 00:16:11 +0530

    Regix

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,28 @@
 const Product = require('../models/product');
 
 getAllProductsStatic = async (req,res)=>{
+    const search ='oo'
     const products = await Product.find({
-        name: 'modern poster'
+        name: {
+            $regex:search,$options:'i'
+        }
     })
     res.status(200).json({products, nbHits:products.length})
 
 }
 
-getAllProducts = (req,res)=>{
-    res.send('hello')
+getAllProducts = async (req,res)=>{
+    const {name,price,company} = req.query;
+    const queryObject = {}
+    if(name){
+        queryObject.name = {$regex:name,$options:'i'}
+    
+    }
+    if(company){
+        queryObject.company = company
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({products,nbHits:products.length})
 }
 
 
